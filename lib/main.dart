fcc9eea9aadcf87843f3c49f2588ae12cb22d593
MorOlesya <dl9morozova@gmail.com>
2026-04-13 20:44:00 +0300

    completed task 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,41 @@
 import 'package:flutter/material.dart';
 
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Container(
+//           decoration: BoxDecoration(
+//             gradient: LinearGradient(
+//               colors: [
+//                 Colors.white,
+//                 Colors.blue,
+//                 Colors.red,
+//               ],
+//               begin: Alignment.topCenter,
+//               end: Alignment.bottomCenter,
+//             ),
+//           ),
+//           child: Center(
+//             child: Text("Hello World",
+//             style: TextStyle(
+//               color: Colors.white,
+//               fontSize: 32,
+//             ),),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.white,
-                Colors.blue,
-                Colors.red,
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ),
-          ),
-          child: Center(
-            child: Text("Hello World",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 32,
-            ),),
-          ),
-        ),
-      ),
-    ),
-  );
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Text("Привет! Меня зовут Олеся \nЯ студент группы ИСП-231", style: TextStyle(
+      fontSize: 20,
+      decoration: TextDecoration.none,
+      color: Colors.black,
+    ),)
+  ));
 }
